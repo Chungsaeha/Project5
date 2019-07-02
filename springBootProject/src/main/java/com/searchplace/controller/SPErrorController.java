08ff2fd189952eaa60eca4edbb3e91c92335b18d
Chungsaeha <saeha4033@naver.com>
2019-07-02 17:16:31 +0900

    Commit

---
+++ springBootProject/src/main/java/com/searchplace/controller/SPErrorController.java
@@ -0,0 +1,41 @@
+package com.searchplace.controller;
+
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+/*
+ * 	에러 페이지.
+ */
+@Controller
+public class SPErrorController implements ErrorController {
+
+	private static final String ERROR_PATH = "/error";
+	
+	@Override
+	public String getErrorPath() {
+		
+		return ERROR_PATH;
+	}
+	
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+        
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("code", status.toString());
+        mv.addObject("msg", httpStatus.getReasonPhrase());
+        mv.addObject("timestamp", new Date());
+        
+        
+        return mv;
+    }
+
+}
